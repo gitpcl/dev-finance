914af34e3410b54ac97edd72cf0643ef6beee561
Pedro Lopes <pedrolopes@Pedros-MacBook-Pro.local>
2021-01-30 20:29:33 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,60 @@
+<h1 align="center">
+    <img src=".github/readme-logo.svg" alt="Logo DevFinance" width="50%" />
+</h1>
+
+<h5 align="center">
+  <b>Your platform to study online.</b> 👨‍🏫
+</h5>
+
+<img  src=".github/demo-devfinance.gif" alt="Gif DevFinance" width="100%" />
+
+## :computer: About the project
+
+This project was developed during Rocketseat's Discover Marathon.
+
+## :floppy_disk: Executing the project
+
+##### 1. You need to clone the repository.
+    $ git clone https://github.com/gitpcl/devfinance.git
+
+##### 2. Installing dependencies.
+    $ npm install --save
+
+##### 3. Running the project.
+    $ npm run dev
+
+##### 4. Open the browser on *URL*.
+    http://localhost:5500/
+
+##### :grey_exclamation: Observation: If you decide to clean the database, delete "database.db" file. After, run the command to recreate it:
+    $ node src/database/db.js
+
+## 💻 Setting up your enviroment:
+
+- [Visual Studio Code](https://code.visualstudio.com/)
+
+## 🚀 VSCode extensions used:
+
+- [Nunjucks Template by eseom](https://marketplace.visualstudio.com/items?itemName=eseom.nunjucks-template)
+- [SQLite by alexcvzz](https://marketplace.visualstudio.com/items?itemName=alexcvzz.vscode-sqlite)
+
+## 👨‍💻 Front-End Technologies used:
+
+- [HTML](https://devdocs.io/html/)
+- [CSS](https://devdocs.io/css/)
+- [Javascript](https://devdocs.io/javascript/)
+
+## 👨‍🔧 Back-End Technologies used:
+
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [SQLite](https://www.sqlite.org/index.html)
+- [Nunjucks](https://mozilla.github.io/nunjucks/)
+
+## :memo: License
+
+This project is under an MIT license. Please check the [LICENSE](LICENSE.md) for more information.
+
+---
+
+Made 💜 by Pedro Lopes
---
+++ scripts.js
@@ -0,0 +1,213 @@
+// BUILD FLOW ==================
+
+// const Modal = {
+//     open() {
+//         // Open modal
+//         // Add class active to modal
+//         document.querySelector('.modal-overlay').classList.add('active');
+
+//     },
+//     close() {
+//         // Close modal
+//         // Remove class active from modal
+//         document.querySelector('.modal-overlay').classList.remove('active');
+//     }
+// }
+
+const Modal = {
+    toggle() {
+        // Open modal
+        // Add class active to modal
+        document.querySelector('.modal-overlay').classList.toggle('active');
+    }
+}
+
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem('dev.finances:transactions')) || [];
+    },
+    set(transactions) {
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions));
+    }
+}
+
+const Transaction = {
+    all: Storage.get(),
+    add(transaction) {
+        Transaction.all.push(transaction);
+
+        App.reload()
+    },
+    remove(index) {
+        Transaction.all.splice(index, 1);
+
+        App.reload()
+    },
+    incomes() {
+        let income = 0;
+        // Get all the transactions
+        // for each transaction,
+        Transaction.all.forEach(transaction=>{
+            // check if it is bigger than zero
+            // if bigger than zero
+            if(transaction.amount > 0) {
+                // add to a variable and return the variable
+                income += transaction.amount;
+            }
+        })
+        return income;
+    },
+    expenses() {
+        let expense = 0;
+        Transaction.all.forEach(transaction=>{
+            if(transaction.amount < 0) {
+                expense += transaction.amount;
+            }
+        })
+        return expense;
+    },
+    total() {
+        return Transaction.incomes() + Transaction.expenses();
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+
+        tr.dataset.index = index;
+
+        DOM.transactionsContainer.appendChild(tr);
+    },
+    innerHTMLTransaction(transaction, index) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+        const amount = Utils.formatCurrency(transaction.amount);
+
+        const html =`
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remove Transaction">
+        </td>
+        `
+        return html
+    },
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes());
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses());
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total());
+    },
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
+    }
+}
+
+const Utils = {
+    formatAmount(value) {
+        value = Number(value.replace(/\,\./g, ""))*100;
+    
+        return value;
+    },
+    formatDate(date){
+        const splittedDate = date.split("-");
+        return `${splittedDate[1]}/${splittedDate[2]}/${splittedDate[0]}`
+    },
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+
+        value = String(value).replace(/\D/g, "");
+
+        value = Number(value)/100;
+
+        value = value.toLocaleString("en", {
+            style: "currency",
+            currency: "USD"
+        })
+
+        return signal + value
+    }
+}
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+    validateFields() {
+        const { description, amount, date } = Form.getValues()
+        
+        if( description.trim() === "" ||
+            amount.trim() === "" ||
+            date.trim() === "") {
+                throw new Error("Please, fill out all fileds.")
+
+            }
+    },
+    formatValues() {
+        let { description, amount, date } = Form.getValues();
+
+        amount = Utils.formatAmount(amount);
+        date = Utils.formatDate(date);
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+    clearFields() {
+        Form.description.value = "";
+        Form.amount.value = "";
+        Form.date.value = "";
+    },
+    submit(event) {
+        event.preventDefault();
+
+        try {
+            Form.validateFields()
+            const transaction = Form.formatValues();
+            Transaction.add(transaction);
+            Form.clearFields()
+            Modal.toggle()
+        } catch (error) {
+            alert(error.message)
+        }
+
+    }
+}
+
+// EXECUTION FLOW ==================
+
+const App = {
+    init(){
+        // Add existing transactions
+        Transaction.all.forEach(DOM.addTransaction)
+
+        DOM.updateBalance()
+
+        Storage.set(Transaction.all)
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    }
+}
+
+App.init()
+
